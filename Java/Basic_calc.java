0edb2246e2a4e80b94d200797abb9127f0e35cd4
Alejandro García <jhetsmangarcia@mail.com>
2025-11-15 08:45:30 -0500

    Add Java file

---
+++ Java/Basic_calc.java
@@ -0,0 +1,27 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Definir variables
+        int num1, num2;
+        int add, subs, mult, div;
+
+        // 2. Inicializar variables
+        num1 = 10;
+        num2 = 5;
+
+        // 3. Procesos
+        add = num1 + num2;
+        subs = num1 - num2;
+        mult = num1 * num2;
+        div = num1 / num2;
+
+        // 4. Salidas
+        System.out.println("Addition: " + add);
+        System.out.println("Substraction: " + subs);
+        System.out.println("Multiplication: " + mult);
+        System.out.println("Division: " + div);
+    }
+}
